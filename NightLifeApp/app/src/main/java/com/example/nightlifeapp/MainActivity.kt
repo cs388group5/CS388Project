3311563faf1dbeb3446e9bb58a51b789f5888a81
jordanNeo <jordan.leebt@gmail.com>
2022-11-29 17:38:58 -0500

    fixed fragments and added login functionality

--- NightLifeApp/app/src/main/java/com/example/nightlifeapp/MainActivity.kt
+++ NightLifeApp/app/src/main/java/com/example/nightlifeapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.nightlifeapp
 
-import android.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.nightlifeapp.fragments.HomeFragment
 import com.example.nightlifeapp.fragments.MapFragment
 import com.example.nightlifeapp.fragments.ProfileFragment
@@ -13,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val fragmentManager: FragmentManager = supportFragmentManager
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -40,7 +42,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (fragmentToShow != null){
+
                 fragmentManager.beginTransaction().replace(R.id.flContainer, fragmentToShow).commit()
+
             }
 
             true
--- NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/LoginFragment.kt
+++ NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/LoginFragment.kt
@@ -1,21 +1,93 @@
 package com.example.nightlifeapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import com.example.nightlifeapp.MainActivity
 import com.example.nightlifeapp.R
+import com.parse.ParseUser
+import com.example.nightlifeapp.fragments.HomeFragment
+
 
 class LoginFragment : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        super.onViewCreated(view, savedInstanceState)
+        if (ParseUser.getCurrentUser() != null){
+            goToHomePage()
+        }
+
+        view.findViewById<Button>(R.id.btnSignup).setOnClickListener {
+            val username = view.findViewById<EditText>(R.id.etUsername).text.toString()
+            val password = view.findViewById<EditText>(R.id.etPassword).text.toString()
+
+            loginUser(username, password)
+        }
+        view.findViewById<Button>(R.id.btnLogin).setOnClickListener {
+            val username = view.findViewById<EditText>(R.id.etUsername).text.toString()
+            val password = view.findViewById<EditText>(R.id.etPassword).text.toString()
+
+            signUpUser(username, password)
+        }
+
+    }
+
+    private fun signUpUser(username: String, password: String){
+        val user = ParseUser()
+
+// Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                goToHomePage()
+                Toast.makeText(requireContext(), "You have registered successfully!", Toast.LENGTH_SHORT).show()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(requireContext(), "Sign up was not successful.", Toast.LENGTH_SHORT).show()
+                // Sign up didn't succeed. Look at the ParseException
+                // to figure out what went wrong
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String){
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                goToHomePage()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(requireContext(), "Error logging in", Toast.LENGTH_SHORT).show()
+                // Signup failed.  Look at the ParseException to see what happened.
+            }})
+        )
+    }
+
+    private fun goToHomePage(){
+
+            parentFragmentManager.beginTransaction().replace(R.id.flContainer, HomeFragment()).commit()
+
     }
 
 
