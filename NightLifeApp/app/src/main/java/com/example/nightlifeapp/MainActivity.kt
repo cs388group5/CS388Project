2433982dd0ac9a1a9d37d038059e83c49fe16fcf
jordanNeo <jordan.leebt@gmail.com>
2022-11-29 23:43:17 -0500

    users can call 911 via the floating action button

--- NightLifeApp/app/src/main/java/com/example/nightlifeapp/MainActivity.kt
+++ NightLifeApp/app/src/main/java/com/example/nightlifeapp/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.example.nightlifeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.nightlifeapp.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val fragmentManager: FragmentManager = supportFragmentManager
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fabEmergency)
+        fab.setOnClickListener { view ->
+            checkPermission()
+
+        }
+
 
         findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
             item ->
@@ -38,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 R.id.nav_profile -> {
                     fragmentToShow = ProfileFragment()
-                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT)
+                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
                 }
             }
 
@@ -51,5 +65,49 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+    }
+    fun callPhone(){
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:" + 911)
+        Toast.makeText(this, "Calling 911", Toast.LENGTH_SHORT).show()
+        startActivity(callIntent)
+
+    }
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CALL_PHONE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CALL_PHONE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    42)
+            }
+        } else {
+            // Permission has already been granted
+            callPhone()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 42) {
+            // If request is cancelled, the result arrays are empty.
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                // permission was granted, yay!
+                callPhone()
+            } else {
+                // permission denied, boo! Disable the
+                // functionality
+            }
+            return
+        }
     }
 }
