f270ab0a6a7a33685cd6fef5df99dd02c9120ad0
ahmedmk323 <ahmedmk323@users.noreply.github.com>
2022-11-30 21:14:45 -0500

    Added Emergency contact class and emergency contact adapter class

--- NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/ProfileFragment.kt
+++ NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/ProfileFragment.kt
@@ -1,10 +1,10 @@
 package com.example.nightlifeapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.nightlifeapp.R
 
 class ProfileFragment : Fragment() {
@@ -16,6 +16,5 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
-
 
 }
