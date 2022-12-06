d88c3600baedc8a464ff64e7f6b38ed06169100d
akahn-git <ak2534@njit.edu>
2022-12-06 15:55:51 -0500

    added markers on the map

--- NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/MapFragment.kt
+++ NightLifeApp/app/src/main/java/com/example/nightlifeapp/fragments/MapFragment.kt
@@ -1,46 +1,144 @@
 package com.example.nightlifeapp.fragments
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.nightlifeapp.Crime
 import com.example.nightlifeapp.R
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+import java.io.IOException
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+
+class MapFragment : Fragment() {
+
+    var allCrimes: MutableList<Crime> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            mMap.clear() //clear old markers
+
+            val googlePlex = CameraPosition.builder()
+                .target(LatLng(37.4219999, -122.0862462))
+                .zoom(10f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null)
+
+        }
+
+        return rootView
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        var mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun bitmapDescriptorFromVector(context: Context?, vectorResId: Int): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(this.requireContext()!!, vectorResId)
+        vectorDrawable!!.setBounds(0, 0, vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight)
+        val bitmap =
+            Bitmap.createBitmap(vectorDrawable.intrinsicWidth, vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 
-}
+    private fun createNewMarker(latitude: Double,longitude: Double, mMap: GoogleMap, title: String, description: String){
+        mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(latitude,longitude))
+                .title(title)
+                .snippet(description)
+        )
+    }
+    //without description
+    private fun createNewMarker(latitude: Double,longitude: Double, mMap: GoogleMap, title: String){
+        mMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(latitude,longitude))
+                .title(title)
+        )
+    }
+
+    private fun getLocationFromAddress(context: Context?, strAddress: String?): LatLng? {
+        val coder = Geocoder(context)
+        val address: List<Address>?
+        var latLngLocation: LatLng? = null
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5)
+            if (address == null) {
+                return null
+            }
+            val location: Address = address[0]
+            latLngLocation = LatLng(location.getLatitude(), location.getLongitude())
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+        return latLngLocation
+    }
+
+    private fun loadMapMarkers(){
+
+        queryCrimes()
+    }
+
+    private fun queryCrimes(){
+
+        //specify which class to query
+        val query: ParseQuery<Crime> = ParseQuery.getQuery(Crime::class.java)
+
+        query.addDescendingOrder("createdAt")
+        query.setLimit(20)
+        query.findInBackground(object : FindCallback<Crime> {
+            override fun done(posts: MutableList<Crime>?, e: ParseException?) {
+                if(e!=null){
+                    Log.e(TAG,"Error fetching posts")
+                }else {
+                    if(posts!=null){
+                        for(post in posts){
+                            Log.i(TAG,"Post: "+post.getDescription() + ", username: "+post.getUser()?.username)
+                        }
+
+                        allCrimes.addAll(posts)
+                    }
+                }
+            }
+
+        })
+    }
+
+    companion object{
+        val TAG="MapFragment"
+    }
+
+}// Required empty public constructor
+
